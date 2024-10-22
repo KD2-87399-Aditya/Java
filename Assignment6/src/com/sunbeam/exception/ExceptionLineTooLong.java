794d20eed804fac5fdb26783e1aba9471c27e7d7
adityakumar293 <76803160+adityakumar293@users.noreply.github.com>
2024-10-22 17:52:13 +0530

    Assignment 6 uploaded

---
+++ Assignment6/src/com/sunbeam/exception/ExceptionLineTooLong.java
@@ -0,0 +1,17 @@
+package com.sunbeam.exception;
+
+public class ExceptionLineTooLong extends Exception {
+
+	public ExceptionLineTooLong() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExceptionLineTooLong(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
---
+++ Assignment6/src/com/sunbeam/tester/AssiTester.java
@@ -0,0 +1,26 @@
+package com.sunbeam.tester;
+
+import java.util.Scanner;
+
+import com.sunbeam.entity.AssQ1_Q2_Q3;
+import com.sunbeam.exception.ExceptionLineTooLong;
+
+public class AssiTester {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AssQ1_Q2_Q3 s = new AssQ1_Q2_Q3();
+		System.out.println("Enter description:");
+		Scanner sc = new Scanner(System.in);
+		String string = sc.next();
+		try {
+			s.setString(string);
+		}catch(ExceptionLineTooLong e) {
+			e.printStackTrace();
+		}
+		System.out.println(s);
+		
+
+	}
+
+
+}
